97f4265acae49dbf997635ac63c42a0b5c4f1397
Bhagya Welivita <bhagyawelivita2021@gmail.com>
2026-03-14 13:54:55 +0530

    initialize Express server and connect to MongoDB

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,17 @@
-console.log("Hello World!!!");
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Middleware
+app.use("/", (req, res, next) => {
+    res.send("It is working");
+});
+
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://Bhagya:Bhagya2001@cluster0.nhnwzgp.mongodb.net/")
+    .then(() => console.log('MongoDB connected'))
+    .then(() => {
+        app.listen(5000);
+    })
+    .catch(err => console.log(err));
